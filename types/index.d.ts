90797172e012735b21b525bc068bf866c719bceb
Donald Pakkies <donaldpakkies@gmail.com>
2021-09-05 16:55:24 +0200

    feat: export the new strRandom helper

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,6 @@
 export var asObject: (object: any) => Object;
 export var config: (notation: string, default$?: any) => any;
+export var dot: (object: any, key: string) => any;
 export var dotNotation: (object: any, key: string) => any;
 export var env: (key: string, default$?: any) => any;
 export var isArray: (object: any) => boolean;
@@ -10,6 +11,7 @@ export var isNumber: (object: any) => boolean;
 export var isObject: (object: any) => boolean;
 export var isString: (object: any) => boolean;
 export var slug: (value: string, separator?: string) => string;
+export var strRandom: (length?: number) => string;
 export var toBoolean: (value: any) => any;
 export var wildcard: (value: string, match: string) => boolean;
 export var without: (object: any, exclude: string[]) => {};
